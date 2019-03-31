9b852994744112ffb5eef59f1aa4f33ebe6f0f30
Joseph Emmanuel Dayo <joseph.dayo@gmail.com>
2019-04-01 07:57:31 +0800

    Add util for getting the OpenCL memory size

--- lib/tensor_stream/opencl.rb
+++ lib/tensor_stream/opencl.rb
@@ -1,6 +1,7 @@
 require "tensor_stream/opencl/version"
 require 'tensor_stream'
 require "tensor_stream/opencl/opencl_evaluator"
+require "tensor_stream/opencl/utils"
 
 module TensorStream
   module Opencl
